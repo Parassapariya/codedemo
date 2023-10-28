fa5da627e60d61c77ff692750470fb274c0dadc3
Paras Sapariya <104016330+Parassapariya@users.noreply.github.com>
2023-10-28 13:07:09 +0530

    phpfiles

---
+++ demo.php
@@ -0,0 +1,191 @@
+<?php
+$con = mysqli_connect("localhost","root","","demo");
+
+
+if(isset($_GET['id']))
+{
+    $id=$_GET['id'];
+    $sql="select * from `test` where `id`='$id'";
+    $res=mysqli_query($con,$sql);
+    $row=mysqli_fetch_assoc($res);
+    $id=$row['id'];
+    $name=$row['name'];
+    $red=$row['red'];
+    $img=$row['image'];
+    $chk=explode(",",$row['chk']);
+    
+    
+}
+
+if(isset($_POST['btnsub']))
+{
+    $red=$_POST['fav_language'];
+
+    $nm=$_POST['nm'];
+    $newnm=str_replace("'","\'","$nm");
+    $checkBox = implode(',', $_POST['q1']);
+    $file_name=$_FILES['img']['name'];
+	$file_temp=$_FILES['img']['tmp_name'];
+	$nm=rand();
+    $end=explode(".",$file_name);
+	$ext=end($end);
+	$nfile=$nm.".".$ext;
+	$file_destination="matirial/".$nfile;
+	if(move_uploaded_file($file_temp,$file_destination))
+	{
+        $sql="INSERT INTO `test`(`red`,`chk`,`name`,`image`) VALUES ('$red','$checkBox','$newnm','$file_destination') ";
+        $res=mysqli_query($con,$sql);
+    }
+}
+
+if(isset($_POST['btnupdate']))
+{
+    $id=$_POST['id'];
+    $red=$_POST['fav_language'];
+
+    $nm=$_POST['nm'];
+    $newnm=str_replace("'","\'","$nm");
+    $checkBox = implode(',', $_POST['q1']);
+
+    if($_FILES['img']['name'] == null)
+    {
+        $img=$_POST['oldimg'];
+        $sql="UPDATE `test` SET `red`='$red',`chk`='$checkBox',`name`='$newnm',`image`='$img' WHERE `id`='$id'";
+        //echo $sql;
+        $res=mysqli_query($con,$sql);
+    }
+    else
+    {
+        $img=$_POST['oldimg'];
+        $file_name=$_FILES['img']['name'];
+        $file_temp=$_FILES['img']['tmp_name'];
+        $nm=rand();
+        $end=explode(".",$file_name);
+        $ext=end($end);
+        $nfile=$nm.".".$ext;
+        $file_destination="matirial/".$nfile;
+        unlink($img);
+        if(move_uploaded_file($file_temp,$file_destination))
+        {
+            $sql="UPDATE `test` SET `red`='$red',`chk`='$checkBox',`name`='$newnm',`image`='$file_destination' WHERE `id`='$id'";
+            echo $sql;
+            $res=mysqli_query($con,$sql);
+        }
+    }
+   
+    
+    header("location:newdemo.php");
+}
+?>
+<html>
+    <body>
+        <form action="demo.php" method="post" enctype="multipart/form-data">
+            <input type="hidden" name="id" value="<?php  echo (isset($id)) ? $id : ''; ?>">
+
+            <!---textbox--->
+            <input type="text" id="tt" value="<?php echo (isset($name)) ? $name : '';?>" name="nm">
+            <label for="tt">name</label><br>
+
+            <!---radio--->
+            <input type="radio" id="html" name="fav_language" value="HTML"
+            <?php
+            if(isset($_GET['id']))
+            {
+                if($red == "HTML")
+                {
+                    echo "checked";
+                }
+            }
+          
+            ?>
+            >
+            <label for="html">HTML</label><br>
+            <input type="radio"  id="html1" name="fav_language" value="html"
+            <?php
+            if(isset($_GET['id']))
+            {
+                if($red == "html")
+                {
+                    echo "checked";
+                }
+            }
+          
+            ?>
+            >
+
+
+            <!---checkbox--->
+            <label for="html1">html</label> <br>
+            
+            <input type="checkbox"  id="html2" name="q1[]" value="q1"
+            <?php
+            if(isset($_GET['id']))
+            {
+                if(in_array("q1",$chk))
+                {
+                    echo "checked";
+                }
+            }
+          
+            ?>
+            >
+            <label for="html2">1</label> <br>
+            <input type="checkbox"  id="html3" name="q1[]" value="q2"
+            <?php
+            if(isset($_GET['id']))
+            {
+                if(in_array("q2",$chk))
+                {
+                    echo "checked";
+                }
+            }
+          
+            ?>
+            >
+            <label for="html3">2</label> <br>
+            <input type="checkbox"  id="html4" name="q1[]" value="q3"
+            <?php
+            if(isset($_GET['id']))
+            {
+             
+            }
+          
+            ?>
+            >
+            <label for="html4">3</label> <br>
+            
+           
+
+            <!--image-->
+            <?php
+            if(isset($_GET['id']))
+            {
+            ?>
+                <div style="margin: 20px;">
+                    <label>OLD IMAGE:</label>
+                    <img src="<?php echo $img ?>" height="100px" width="100px">
+                </div>
+            <?php
+            }
+            ?>
+            <input type="hidden" name="oldimg" value="<?php echo $img ?>" id="img"><br><br>
+            <input type="file" name="img" id="img"><br><br>
+
+            <!--button chnage task-->
+            <?php
+            if(isset($_GET['id']))
+            {
+            ?>
+              <input type="submit" name="btnupdate" value="update">
+            <?php
+            }else
+            {
+                ?>
+                <input type="submit" name="btnsub" >
+                <?php
+            }
+            ?>
+          
+        </form>
+    </body>
+</html>
---
+++ newdemo.php
@@ -0,0 +1,121 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<?php
+$con = mysqli_connect("localhost","root","","demo");
+
+
+
+if(isset($_GET['id']))
+{
+     $sql = "DELETE FROM `test` WHERE id=".$_GET['id'];
+        $res=mysqli_query($con,$sql);
+	 echo 'Deleted successfully.';
+}
+
+$limit = 3; 
+$sql="select * from `test`";
+$res=mysqli_query($con,$sql);
+$total_rows = mysqli_num_rows($res);    
+$total_pages = ceil ($total_rows / $limit); 
+   
+// update the active page number
+
+if (!isset ($_GET['page']) ) {  
+
+    $page_number = 1;  
+
+} else {  
+
+    $page_number = $_GET['page'];  
+
+}    
+ // get the initial page number
+
+ $initial_page = ($page_number-1) * $limit;   
+
+ // get data of selected rows per page    
+
+ $getQuery = "SELECT * FROM `test` LIMIT  $initial_page ,  $limit";  
+//echo $getQuery;
+ $result = mysqli_query($con, $getQuery);       
+?>
+<table border="2px">
+    <?php
+    $i=0;
+    while($row=mysqli_fetch_assoc($result))
+    {
+        if($i % 3 == 0)
+        {
+    ?>
+    <tr>
+        <?php
+        }
+        ?>
+       
+        <td>
+        <img src="<?php echo $row['image'] ?>" height="300px" width="300px"><br>
+        <?php echo $row['name'] ?><br>
+        <button class="edit" ><a style="text-decoration:none" href="demo.php?id=<?php echo $row['id']  ?>">edit</a></button>
+        <button class="remove" data-id="<?php echo $row['id']  ?>">delete</button></td>
+    <?php
+    $i++;
+    if($i % 3 == 0)
+    {
+      ?>
+      </tr>
+      <?php  
+    }
+    ?>
+    
+    <?php
+    
+    }
+      // show page number with link   
+
+      for($page_number = 1; $page_number<= $total_pages; $page_number++) {  
+
+        echo '<a href = "newdemo.php?page=' . $page_number . '">' . $page_number . ' </a>';  
+
+    }    
+    ?>
+</table>
+<script>
+     $(".remove").click(function(){
+
+    var id = $(this).data("id");
+
+
+
+if(confirm('Are you sure to delete this record ?'))
+
+{
+
+    $.ajax({
+
+       url: 'newdemo.php',
+
+       type: 'GET',
+
+       data: {id: id},
+
+       error: function() {
+
+          alert('Something is wrong');
+
+       },
+
+       success: function(data) {
+
+            $("#"+id).remove();
+            location.reload();
+
+       }
+
+    });
+
+}
+
+});
+
+
+
+</script>
